dfab31a8220c1a654798b91b177687b18d834242
yoophoon <2420074212@qq.com>
2024-02-25 09:12:52 +0800

    添加无限翻页功能

--- tampermonkey/biquge365.net阅读背景调整.js
+++ tampermonkey/biquge365.net阅读背景调整.js
@@ -10,7 +10,7 @@
 // @author       yoophoon
 // @homepage     https://github.com/yoophoon
 // @match        https://www.biquge365.net/chapter/*
-// @run-at document-end
+// @run-at       document-end
 // @updateURL    https://raw.githubusercontent.com/yoophoon/HOW-TO-USE-THIS/main/tampermonkey/biquge365.net%E9%98%85%E8%AF%BB%E8%83%8C%E6%99%AF%E8%B0%83%E6%95%B4.js
 // @downloadURL  https://raw.githubusercontent.com/yoophoon/HOW-TO-USE-THIS/main/tampermonkey/biquge365.net%E9%98%85%E8%AF%BB%E8%83%8C%E6%99%AF%E8%B0%83%E6%95%B4.js
 // @license      MIT
@@ -21,52 +21,25 @@
     //GM_addStyle对脚本无效单纯插入一段CSS验证该函数的功能性
     //GM_addStyle(".uselessStyle,.usefulStyle{height:150px !important}")
     setUserCSS('userCSS')
-    //隐藏功能菜单
-    document.querySelector('header.head').style.display = 'none'
-    //隐藏报错按钮 底部和底部都有这种功能用for循环处理掉
-    let errorBar = document.querySelectorAll('.baocuo')
-    for (let i = 0; i < errorBar.length; i++) {
-        errorBar[i].style.display = 'none'
+    let webSelfScript = document.head.querySelectorAll('script')
+    for (i = 0; i < webSelfScript.length; i++) {
+        webSelfScript[i].remove()
     }
-
-    //隐藏顶部功能菜单
-    //调整字号、关灯功能
-    document.querySelector('#neirong > div.gongneng1').style.display = 'none'
-    //章节目录功能
-    document.querySelector('div#fanye').style.display = 'none'
-    //文字广告
-    document.querySelector('#txt > p').style.display = 'none'
+    document.body.style.backgroundColor = '#999999'
+    document.body.style.color = '#cc9966'
+    let showContent = document.createElement('div')
+    showContent.setAttribute('id', 'contentPanel')
 
 
-    //设置body背景
-    let page = document.querySelector('#moshi')
-    page.style.backgroundColor = '#999999'
+    //here
+    let chapterID = document.location.href.split('/')[5].split('.')[0]
+    showContent.appendChild(setContent(chapterID, document.body))
+    document.body.appendChild(showContent)
 
-    //设置文章区域背景
-    let content = document.querySelector('#neirong')
-    content.style.cssText = 'font-family: "microsoft yahei";color:#cc9966;background-color:#666666;border:none;margin-bottom:20px;box-sizing:border-box'
-    content.querySelector('h1').style.cssText = 'color:#cc9966;font:\'Microsoft YaHei\',Arial,Helvetica,Sans-Serif'
-    content.querySelector('br').style.display = 'none'
-    //调整底部翻页功能的样式
-    let pageTurningBar = document.querySelectorAll('div#fanye1 > ul > li')
-    for (let i = 0; i < pageTurningBar.length; i++) {
-        pageTurningBar[i].style.cssText = 'background-color:#666666;border:none'
-        pageTurningBar[i].querySelector('a').style.cssText = 'color:#cc9966;border-bottom:3px solid #cc9966;width:100px;padding-bottom:5px'
-    }
-
-    //隐藏底部功能
-    //推荐内容
-    document.querySelector('#neirong > div.like').style.display = 'none'
-    //页脚内容
-    document.querySelector('div.footer').style.display = 'none'
 
     //创建侧栏显示最一定数量的最新章节和当前章节的前后章节
     let sideBar = document.createElement('ul')
-
-    sideBar.style.cssText = 'width:250px;position:fixed;top:20%;left:calc(50% - 720px);box-sizing:border-box'
-
-    //给侧栏添加全文目录按钮
-
+    sideBar.style.cssText = 'width:250px;position:fixed;top:20%;left:calc(50% - 750px);box-sizing:border-box;overflow:hidden;'
     //设置鼠标进入退出效果
     sideBar.onmouseenter = function () {
         sideBar.style.backgroundColor = '#666666'
@@ -77,102 +50,15 @@
     }
 
     let bookID = document.location.href.split('/')[4]
-    //设置侧栏目录
-    let directory = document.createElement('a')
-    directory.style.cssText = 'color:#cc9966;font-size:25px;padding-bottom:5px;display:block;text-align:center'
-    directory.href = `https://www.biquge365.net/newbook/${bookID}/`
-    directory.innerText = '全部章节'
-    sideBar.appendChild(directory)
+    let allChaptersInfo = getAllChaptersInfo(bookID)
     document.body.appendChild(sideBar)
-    getChaptersInfo(bookID, document.location.href, sideBar, 5)
+    //加载侧栏章节信息
+    setChapterPanel(sideBar, chapterID, allChaptersInfo, 5)
+    //动态加载章节内容
+    preloadNextChapters(sideBar, showContent, allChaptersInfo, 5)
+
 })();
 
-
-/**
- * 
- * @param {string} bookID 网站的书籍标识
- * @param {string} currentChapter 当前章节标识
- * @param {Node} parentNode 挂载的元素节点
- * @param {number} nodeNum 需要挂载的数量
- */
-function getChaptersInfo(bookID, currentChapter, parentNode, nodeNum) {
-    const chaptersURL = `https://www.biquge365.net/newbook/${bookID}/`
-    const parser = new DOMParser()
-    const currentChapterStyle = {
-        li: 'width:100%;margin:2px;margin-left:4px',
-        a: 'color:#cc9966;border-bottom:1px solid #cc9966;padding:1px;padding-left:10px;border-left:4px solid #666666;margin-left:8px'
-    }
-    const otherChaptersStyle = {
-        li: 'width:100%;margin:2px',
-        a: 'color:#cc9966;padding:1px;padding-left:10px;border-left:2px solid #666666'
-    }
-    fetch(chaptersURL)
-        .then(response => response.text())
-        .then(resText => {
-            //将获取的文本转换成了DOM对象
-            let chapterPage = parser.parseFromString(resText, 'text/html')
-            //处理最新章节
-            let newestChapters = chapterPage.querySelectorAll(" ul.xinchapter > li")
-            for (let i = 0; i < (newestChapters.length > nodeNum ? nodeNum : newestChapters.length); i++) {
-                parentNode.appendChild(newestChapters[i])
-                if (i == ((newestChapters.length = nodeNum ? nodeNum : newestChapters.length) - 1)) {
-                    let hrIndirectory = document.createElement('hr')
-                    //hrIndirectory.style.cssText = 'background-color:#999444;height:1px;border:none'
-                    parentNode.appendChild(hrIndirectory)
-                }
-                setStyle(newestChapters[i], currentChapter, currentChapterStyle, otherChaptersStyle)
-            }
-            //处理前后章节
-            let currentChapters = chapterPage.querySelectorAll(" ul.info > li")
-            let maxNumChapters = currentChapters.length
-            //console.log(currentChapters.length)
-            if (maxNumChapters <= nodeNum) {
-                return
-            }
-            //console.log(currentChapters.length - nodeNum)
-            //外循环确定当前章节在全文位置
-            for (let i = maxNumChapters - 1; i >= 0; i--) {
-                if (currentChapters[i].querySelector('a').href == currentChapter) {
-                    //内循环定位
-                    let j
-                    //判断前后章节的最新章和全文最新章的位置关系 取小值 如果是相等则取等值减一防止元素重复
-                    if ((i + parseInt(nodeNum / 2)) > (maxNumChapters - nodeNum)) { j = maxNumChapters - nodeNum - 1 }
-                    if ((i + parseInt(nodeNum / 2)) < (maxNumChapters - nodeNum)) { j = i + parseInt(nodeNum / 2) }
-                    if ((i + parseInt(nodeNum / 2)) == (maxNumChapters - nodeNum)) { j = i + parseInt(nodeNum / 2) - 1 }
-                    console.log(j)
-                    for (; j >= (0 > (i - parseInt(nodeNum / 2)) ? 0 : (i - parseInt(nodeNum / 2))); j--) {
-                        parentNode.appendChild(currentChapters[j])
-                        setStyle(currentChapters[j], currentChapter, currentChapterStyle, otherChaptersStyle)
-                    }
-                    return
-                }
-            }
-        })
-}
-/**
- * 
- * @param {*} Node 操作节点
- * @param {string} currentChapter 当前章节信息
- * @param {*} currentStyle 当前章节样式
- * @param {*} otherStyle 其他章节样式
- */
-function setStyle(Node, currentChapter, currentStyle, otherStyle) {
-    Node.style.cssText = otherStyle.li
-    let chapterInfo = Node.querySelector('a')
-    chapterInfo.style.cssText = otherStyle.a
-    //如果是最新章节的处理
-    if (currentChapter == chapterInfo.href) {
-        Node.style.cssText = currentStyle.li
-        chapterInfo.style.cssText = currentStyle.a
-        chapterInfo.href = '#'
-    }
-    Node.onmouseenter = function () {
-        Node.style.cssText = currentStyle.li
-    }
-    Node.onmouseleave = function () {
-        Node.style.cssText = otherStyle.li
-    }
-}
 
 /**
  * 
@@ -185,7 +71,7 @@ function setUserCSS(userCSS) {
     document.head.appendChild(styleEl)
     //类似documen.style.cssText
     //userCSS.sheet.addRule('.userCSS::after', 'color:green')
-    styleEl.sheet.insertRule(`hr{
+    styleEl.sheet.insertRule(`.splitInChapterPanel{
         border: none;
         border-top: 2px double #cc9966;
         color:#333;
@@ -195,6 +81,231 @@ function setUserCSS(userCSS) {
         margin-top:11px;
         margin-bottom:8px;
     }`)
-    styleEl.sheet.insertRule(`hr::after {color:#666666;background-color: #cc9966;content:'最新章节↑§↓最近章节';padding:0 4px;position:relative;font-size:12px;top:-13px;border-radius:6px}`)
+    styleEl.sheet.insertRule(`.splitInChapterPanel::after {color:#666666;background-color: #cc9966;content:'最新章节↓§↑最近章节';padding:0 4px;position:relative;font-size:12px;top:-13px;border-radius:6px}`)
+
+    styleEl.sheet.insertRule(`.chapterInfo{
+        width:230px;
+        border-left: 2px solid #666666;
+        color: #cc9966;
+        padding-left: 10px;
+        margin: 5px 0px;}
+    `)
+
+    styleEl.sheet.insertRule(`.chapterInfo:hover {
+        margin-left: 10px;}
+    `)
+
+    styleEl.sheet.insertRule(`.currentChapterInfo {
+        margin:5px 0px 5px 25px;
+        border-bottom: 2px solid #cc9966;
+        color:#cc9966}
+    `)
+
+    styleEl.sheet.insertRule(`.currentChapterInfo:hover {
+        margin-left: 35px;}
+    `)
+
+    styleEl.sheet.insertRule(`.currentChapterInfo::before {
+        content: ">";
+        font-size: 30px;
+        display: block;
+        position: relative;
+        top: 10px;
+        right: 20px;
+        color: #666666;
+        line-height:0px;}
+    `)
+    styleEl.sheet.insertRule(`#contentPanel{
+        width: 1000px;
+        height:100vh;
+        background-color: #666666;
+        margin-top: 0;
+        padding-top: 0;
+        margin: 0 auto;
+        overflow: scroll;}`
+    )
+    styleEl.sheet.insertRule(`#contentPanel::-webkit-scrollbar{
+        display:none;}`
+    )
 }
 
+/**
+ * 
+ * @param {*} id 章节ID
+ * @param {*} node 传入的包含章节内容的DOM
+ * @returns 放回仅包含标题和正文的DOM
+ */
+function setContent(chapterID, node) {
+    //章节标题样式
+    const styleTitle = `
+    margin: 0;
+    padding:50px 0;
+    text-align: center;`
+    //章节正文样式
+    const styleContent = `
+    font-family: "microsoft yahei";
+    font-size: 18px;
+    line-height: 50px;
+    box-sizing: border-box;
+    padding: 0px 50px;
+    `
+    let currentChapter = document.createElement('div')
+    currentChapter.setAttribute('id', chapterID)
+    currentChapter.setAttribute('class', 'content')
+    //window.location.href = window.location.href.split('#')[0] + '#' + chapterID
+    let currentContent = node.querySelector('#txt')
+    let currentTitle = node.querySelector('h1')
+    currentTitle.style.cssText = styleTitle
+    currentContent.style.cssText = styleContent
+    currentContent.removeAttribute('id')
+    currentContent.removeAttribute('class')
+    currentContent.removeChild(currentContent.firstElementChild)//文字广告
+    currentContent.removeChild(currentContent.firstElementChild)//第一个换行
+    node.innerHTML = ''
+    currentChapter.appendChild(currentTitle)
+    currentChapter.appendChild(currentContent)
+    return currentChapter
+}
+
+/**
+ * 
+ * @param {*} chapterPanel 侧栏显示章节信息容器
+ * @param {*} contentPanel 显示章节正文信息容器
+ * @param {*} allChaptersInfo 所有的章节信息
+ * @param {*} nodeNum 最新章节数量=nodeNum、前后章节数量=parseInt(nodeNum)、预加载数量=parseInt(nodeNum)
+ * @description 简单版无限阅读
+ */
+function preloadNextChapters(chapterPanel, contentPanel, allChaptersInfo, nodeNum) {
+    const parser = new DOMParser()
+    contentPanel.addEventListener("scrollend", (event) => {
+        allChaptersInfo.then(data => {
+            let indexOfCurrentChapter = parseInt(window.location.href.split('#')[2])
+            for (let i = 1; i <= parseInt(nodeNum / 2); i++) {
+                let tempIndex = indexOfCurrentChapter + i
+                if (tempIndex >= data.length) {
+                    break
+                }
+                if (data[tempIndex][2] == 1) {
+                    fetch(data[indexOfCurrentChapter + i][1])
+                        .then(response => response.text())
+                        .then(resText => {
+                            let chapterPage = parser.parseFromString(resText, 'text/html')
+                            data[indexOfCurrentChapter + i][2] = setContent(data[indexOfCurrentChapter + i][1].split('/')[5].split('.')[0], chapterPage)
+                            contentPanel.appendChild(data[indexOfCurrentChapter + i][2])
+                        })
+                }
+            }
+        })
+        allChaptersInfo.then(data => {
+            let theFirstChapterInChapterPanel = contentPanel.querySelector('.content')
+            //let currentChapterUrl = `https://www.biquge365.net/chapter/${window.location.href.split('/')[4]}/${theFirstChapterInChapterPanel.getAttribute('id')}.html`
+
+            if (contentPanel.scrollTop - theFirstChapterInChapterPanel.scrollHeight > 0) {
+                let indexOfCurrentChapter = parseInt(window.location.href.split('#')[2]) + 1
+                if (indexOfCurrentChapter >= data.length) return
+                let chapterID = data[indexOfCurrentChapter][1].split('/')[5].split('.')[0]
+                window.location.href = `${window.location.href.split('#')[0]}#${chapterID}#${indexOfCurrentChapter}`
+                setChapterPanel(chapterPanel, chapterID, allChaptersInfo, nodeNum)
+                theFirstChapterInChapterPanel.remove()
+            }
+
+        })
+
+    });
+}
+
+/**
+ * 
+ * @param {*} bookID 书籍ID
+ * @returns 所有章节标题及链接
+ */
+async function getAllChaptersInfo(bookID) {
+    const chaptersURL = `https://www.biquge365.net/newbook/${bookID}/`
+    const parser = new DOMParser()
+    let allChaptersInfo = new Array()
+
+    await fetch(chaptersURL)
+        .then(response => response.text())
+        .then(resText => {
+            let chapterPage = parser.parseFromString(resText, 'text/html')
+            let chapterLi = chapterPage.querySelectorAll(" ul.info > li> a")
+            for (i = 0; i < chapterLi.length; i++) {
+                let tempInfo = new Array(3)
+                tempInfo[0] = chapterLi[i].getAttribute('title')
+                tempInfo[1] = 'https://www.biquge365.net' + chapterLi[i].getAttribute('href')
+                tempInfo[2] = 1
+                allChaptersInfo.push(tempInfo)
+            }
+        })
+    return allChaptersInfo
+}
+
+/**
+ * 
+ * @param {*} chapterPanel 侧栏章节信息挂载对象
+ * @param {*} currentChapterID 当前章节ID用于生成前后章节信息
+ * @param {*} allChaptersInfo 所有章节信息
+ * @param {*} nodeNum 显示章节数量
+ */
+function setChapterPanel(chapterPanel, currentChapterID, allChaptersInfo, nodeNum) {
+    allChaptersInfo.then(data => {
+        //清空侧栏目录内容
+        chapterPanel.innerHTML = ''
+        //设置侧栏目录
+        let directory = document.createElement('a')
+        directory.style.cssText = 'color:#cc9966;font-size:25px;padding-bottom:5px;display:block;text-align:center'
+        directory.href = `https://www.biquge365.net/newbook/${window.location.href.split('/')[4]}/`
+        directory.innerText = '全部章节'
+        chapterPanel.appendChild(directory)
+        //处理前后章节信息
+        //外循环定位
+        let indexOfCurrentChapter, j
+        for (let i = data.length - 1; i >= 0; i--) {
+            //如果href有当前章节的index信息就直接用index信息加速
+            if (window.location.href.split('#')[2] != undefined) {
+                i = i - parseInt(window.location.href.split('#')[2]) > nodeNum ? parseInt(window.location.href.split('#')[2]) : i
+            }
+            if (currentChapterID == data[i][1].split('/')[5].split('.')[0]) {
+                indexOfCurrentChapter = i
+                if (window.location.href.split('#')[1] != undefined) {
+                    window.location.href = `${window.location.href.split('#')[0]}#${window.location.href.split('#')[1]}#${indexOfCurrentChapter}`
+                } else {
+                    window.location.href += `#${currentChapterID}#${indexOfCurrentChapter}`
+                }
+                //内循环生成元素
+                for (j = (0 > (i - parseInt(nodeNum / 2)) ? 0 : (i - parseInt(nodeNum / 2)));
+                    j <= ((data.length - 1) > (i + parseInt(nodeNum / 2)) ? (i + parseInt(nodeNum / 2)) : (data.length - 1)); j++) {
+                    let chapterInfo = document.createElement('a')
+                    chapterInfo.setAttribute('class', 'chapterInfo')
+                    if (i == j) {
+                        chapterInfo.setAttribute('class', 'chapterInfo currentChapterInfo')
+                    }
+                    chapterInfo.innerText = data[j][0]
+                    chapterInfo.href = data[j][1]
+                    chapterPanel.appendChild(chapterInfo)
+                }
+                break
+            }
+        }
+        //插入分割线
+        setSplitLine(chapterPanel)
+        //处理最新章节信息
+        for (i = (indexOfCurrentChapter + parseInt(nodeNum / 2) + 1 < data.length - nodeNum ? data.length - nodeNum : indexOfCurrentChapter + parseInt(nodeNum / 2) + 1); i < data.length; i++) {
+            let chapterInfo = document.createElement('a')
+            chapterInfo.setAttribute('class', 'chapterInfo')
+            chapterInfo.innerText = data[i][0]
+            chapterInfo.href = data[i][1]
+            chapterPanel.appendChild(chapterInfo)
+        }
+    })
+}
+
+/**
+ * 
+ * @param {*} parentNode 分割线挂载对象
+ */
+function setSplitLine(parentNode) {
+    let hrIndirectory = document.createElement('hr')
+    hrIndirectory.setAttribute('class', 'splitInChapterPanel')
+    parentNode.appendChild(hrIndirectory)
+}
